249d005733f91fd8d1e408f1a8f8f83ea73f4f71
welch16 <welch@stat.wisc.edu>
2015-09-25 11:46:43 -0500

    added additional arguments to reads class

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -5,16 +5,26 @@ NULL
 ##'
 ##' Contains the reads obtained in a ChIP - seq experiment separated by strand and then by chromosome. It has one component for each strand which are object of the data.table class with a match column to identify the regions
 ##'
+##' @slot readsFile Character vector with the name of the reads file to be used
 ##' @slot readsF List of data.table objects containing the reads of the ChIP - Seq experiment that have + strand.
 ##' @slot readsR List of data.table object containning the reads of the ChIP - Seq experiment that have - strand
-##' @seealso \code{\link{loadReads}}
+##' @slot nReads Numeric value with the number of reads 
 ##'
 ##' @name reads-class
 ##' @rdname reads-class
 ##' @exportClass reads
 setClass("reads",
-  representation(readsF = "list",readsR = "list"),
-  prototype = prototype(readsF = list(),readsR = list()))
+  representation(
+    readsFile = "character",
+    readsF = "list",
+    readsR = "list",
+    nReads = "numeric"),
+  prototype = prototype(
+    readsFile = "",
+    readsF = list(),
+    readsR = list(),
+    nReads = 0
+    ))
 
 setValidity("reads",
   function(object){
