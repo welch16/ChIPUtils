fa24f2f98d9dc750d1a50b6c4c44081cf3e6ea89
Rene Welch <welch16@gmail.com>
2015-10-11 21:59:27 -0500

    Added a function that returns the available chrom sizes in the package

--- R/utils.R
+++ R/utils.R
@@ -101,3 +101,29 @@ dt2ir <- function(dt)
   ir <- IRanges(start = dt[,(start)],end = dt[,(end)])
   return(ir)
 }
+
+##################################################################################
+
+##' Returns the chromosome sizes available in the package
+##'
+##' @description Returns the chromosome sizes stored in the \code{inst/extdata/chrom.size} directory of the ChIPUtils package
+##'
+##' @export
+##'
+##' @return A character vector
+##'
+##' @rdname possible_chrom_sizes
+##' @name possible_chrom_sizes
+##'
+##' @examples
+##'
+##' possible_chrom_sizes()
+
+possible_chrom_sizes <- function()
+{
+  dr <- system.file("inst","extdata","chrom.sizes",package = "ChIPUtils")
+  sizes <- list.files(dr)
+  chrom <- sapply(strsplit(sizes,".",fixed = TRUE),function(x)x[1])
+  return(chrom)
+
+}
