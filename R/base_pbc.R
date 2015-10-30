10835a0eafa3dee4ab2e6d7fb7c80bba4b421b73
Rene Welch <welch16@gmail.com>
2015-10-30 13:50:23 -0500

    Added code to calculate PCR bottleneck coefficient for Paired End data

--- R/base_pbc.R
+++ R/base_pbc.R
@@ -21,7 +21,7 @@
 PBC <- function(reads)
 {
   stopifnot(class(reads) == "reads")
-  if(!isPET(reads)){
+  if(!isPET(reads)){ ## SET
     tagsF <- lapply(readsF(reads),
       function(x){
         out <- x[,length(end),by = start]
@@ -32,12 +32,26 @@ PBC <- function(reads)
         out <- x[,length(start),by = end]
         setnames(out,names(out),c("position","tags"))
         return(out)})
-    n1 <- sum(sapply(tagsF,function(x)nrow(x[tags == 1]))) +
-        sum(sapply(tagsR,function(x)nrow(x[tags == 1])))
-    n2 <- sum(sapply(tagsF,nrow)) + sum(sapply(tagsR,nrow))
-  }else{
-    n1 <- 0
-    n2 <- 1
+  }else{ ## PET
+    rF <- readsF(reads)
+    rR <- readsR(reads)
+    rF <- lapply(rF,function(x)data.table(read = x[,paste0(start,"-",end)]))
+    rR <- lapply(rR,function(x)data.table(read = x[,paste0(start,"-",end)]))
+    rF <- lapply(rF,function(x)x[,V2 := 1])
+    rR <- lapply(rR,function(x)x[,V2 := 1])  
+    tagsF <- lapply(rF,function(x){
+      out <- x[,length(V2), by = read]
+      setnames(out,names(out),c("position","tags"))
+      return(out)
+    })
+    tagsR <- lapply(rR,function(x){
+      out <- x[,length(V2),by = read]
+      setnames(out,names(out),c("position","tags"))
+      return(out)
+    })
   }
+  n1 <- sum(sapply(tagsF,function(x)nrow(x[tags == 1]))) +
+    sum(sapply(tagsR,function(x)nrow(x[tags == 1])))
+  n2 <- sum(sapply(tagsF,nrow)) + sum(sapply(tagsR,nrow))
   return(n1 / n2)                 
 }
