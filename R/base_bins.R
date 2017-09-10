9ffe56990ce4bc8466b34c5a9b5fb3df43955bc6
welch6 <welch16@gmail.com>
2017-09-10 00:08:57 -0500

    added code to generate MAplot

--- R/base_bins.R
+++ R/base_bins.R
@@ -84,14 +84,10 @@ createBins <- function(binSize, chipdata = NULL , chrom = NULL, fragLen = 1)
 }
 
 
-
-
-###############################################################################333
-
-##' Creates a quick hexbin plot to check the relationship between two reads objects
+##' Creates a quick hexbin plot to check the relationship between two ChIPdata objects
 ##' 
 ##' @description Creates a quick hexbin plot to visualize the relationship between 
-##' two reads objects
+##' two ChIPdata objects
 ##' 
 ##' @param x A \code{ChIPdata} object that corresponds to the x-axis
 ##' @param y Another \code{ChIPdata} object, this one correspond to the y-axis
@@ -155,6 +151,66 @@ hexbinPlot <- function(x,y,binSize,log = FALSE,nrBins =40,chrom = NULL, fragLen 
   plot
 }
 
+##' Creates a quick MA plot to check the relationship between two reads objects
+##' 
+##' @description Creates a quick MA plot to visualize the relationship between 
+##' two reads objects
+##' 
+##' @param x A \code{ChIPdata} object that corresponds to the x-axis
+##' @param y Another \code{ChIPdata} object, this one correspond to the y-axis
+##' @param binSize A integer value used to partition the chromosome 
+##' @param nrBins Integer value with the number of bins used to build the plot. The default value is 100
+##' @param chrom A GRanges object specifying the genome to bin. The maximum length used to create the bins
+##' is gonna be used the integer part of (chromLen / fragLen) times fragLen for each chromosome.
+##' @param fragLen An integer value used to extend the fragments. The default value is one, to count only the 5' ends 
+##' that overlaps the bins
+##'
+##' @export
+##' 
+##' @return A ggplot object with the hexbin plot
+##' 
+##' @rdname MAPlot
+##' @name MAPlot
+MAPlot <- function(x,y,binSize,log = FALSE,nrBins =40,chrom = NULL, fragLen = 1)
+{
+  stopifnot(class(x) == "ChIPdata",
+            class(y) == "ChIPdata")
+  stopifnot(binSize > 0,
+            fragLen > 0,
+            nrBins > 1)
+  
+  if(is.null(chrom)){
+    
+    chromx <- genomeFromChIPdata(x)
+    chromy <- genomeFromChIPdata(y)
+    
+    chrom <- GenomicRanges::intersect(chromx,chromy)
+    
+  }
+  
+  binsx <- createBins(binSize,x,chrom = chrom,fragLen = fragLen)
+  binsy <- createBins(binSize,y,chrom = chrom,fragLen = fragLen)
+  
+  plotData <- tibble(
+    x = mcols(binsx)$tagCounts,
+    y = mcols(binsy)$tagCounts
+  ) %>% 
+    mutate(
+      M = 0.5 * (log2(x) + log2(y)),
+      A = log2(x) - log2(y)
+    )
+  
+  
+
+  pal <- viridis::viridis(100, option = "D")
+  plot <- plotData %>% 
+    ggplot(aes(M,A))+
+    stat_binhex(bins = nrBins)+
+    scale_fill_gradientn(colours = pal,trans = 'log10',
+                         labels=trans_format('log10',math_format(10^.x)) )
+  
+  plot
+}
 
 
 
